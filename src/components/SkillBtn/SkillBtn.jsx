e05fb4a28926d2f6e6fa41848c9aeb256f43532e
Tifenn-Lamure <tifenn.lamure@hotmail.com>
2024-07-01 22:38:51 +0200

    Fixed button text color

--- src/components/SkillBtn/SkillBtn.jsx
+++ src/components/SkillBtn/SkillBtn.jsx
@@ -3,7 +3,7 @@ function SkillBtn ({SkillBtnText, SkillBtnTextColor = 'text-white', BtnColorBack
   return (
     <>
       <div className="relative">
-        <div className={`text-white text-2xl text-center ${SkillBtnTextColor} w-48 ${BtnColorFront} rounded-lg border-black border-4 relative z-10`}>{SkillBtnText}</div>
+        <div className={`text-2xl text-center ${SkillBtnTextColor} w-48 ${BtnColorFront} rounded-lg border-black border-4 relative z-10`}>{SkillBtnText}</div>
         <div className={`w-48 h-10 ${BtnColorBack} rounded-lg border-black border-4 absolute z-0 top-2 left-2`} ></div>
       </div>
     </>
