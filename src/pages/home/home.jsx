b96d648aebd6296c194f3c06a22e94ea814128c5
Tifenn-Lamure <tifenn.lamure@hotmail.com>
2024-07-03 17:50:39 +0200

    add BG and drawing + animate it in the home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import TiaraPic from "./../../assets/img/TiaraPic.PNG";
+import TifennDrawing from "./../../assets/img/TifennDrawing.PNG";
+import CloudBg from "./../../assets/img/CloudBG.webp";
 
 const Home = () => {
   return(
     <>
-      <div className="h-[100%] bg-light-violet flex items-center justify-around">
-        <div className="flex flex-col items-center">
+      <div className="h-[100vh] bg-light-violet flex items-center justify-around relative">
+        <img src={CloudBg} alt="drawing of the sky with clouds" className="w-full absolute z-0 object-fill" />
+        <div className="h-full w-full bg-black z-[5] absolute mix-blend-multiply opacity-[72%]"></div>
+        <div className="flex flex-col items-center z-20">
           <h1 className="text-7xl text-white">Tifenn LAMURE</h1>
-          <h2 className="text-5xl text-white">Portfolio</h2>
+          <h2 className="text-5xl text-white">Front-end developer</h2>
           <div className="relative mt-20">
             <Link to={`/resume`} className="bg-dark-violet h-16 w-80 flex items-center justify-center rounded-lg border-4 border-black absolute z-10 m-10 bottom-2 right-2 text-3xl text-white font-light hover:bottom-0 hover:right-0">All about me</Link>
             <div className="bg-white h-16 w-80 flex items-center justify-center rounded-lg border-4 border-black z-0 m-10"></div>
@@ -18,7 +21,7 @@ const Home = () => {
             <div className="bg-white h-16 w-80 flex items-center justify-center rounded-lg border-4 border-black z-0"></div>
           </div>
         </div>
-        <img src={TiaraPic} className="w-1/4" alt="drawing of me"/>
+        <img src={TifennDrawing} className="w-[45%] animate-[float_6s_infinite] z-40" alt="drawing of me"/>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
         'light-violet': '#9595DD',
         'dark-blue-violet': '#383DB4',
         'very-light-violet': '#D7DCFF',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { 
+            transform: 'translateY(6%)',
+            transitionTimingFunction: 'ease-in-out',
+            rotate: "-16deg"
+          },
+          '50%': { 
+            transform: 'translateY(0px)',
+            transitionTimingFunction: 'ease-in-out',
+            rotate: "-16deg"
+          },
+        }
       }
     },
   },
